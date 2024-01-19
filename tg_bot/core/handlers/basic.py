1600b2eafa6c56c36cb6bad9e6fb6d8c1fb62da5
Ivan Bondarenko <krenkroyt@gmail.com>
2024-01-19 20:22:55 +0400

    tg_bot filters develop

--- tg_bot/core/handlers/basic.py
+++ tg_bot/core/handlers/basic.py
@@ -13,3 +13,9 @@ async def get_start(message: Message, bot: Bot):
     await message.reply(
         f"<tg-spoiler>Hello {message.from_user.first_name}, welcome to my bot3</tg-spoiler>"
     )
+
+
+async def get_photo(message: Message, bot: Bot):
+    await message.answer(f"Great! Your sand image, i'm save it here!")
+    file = await bot.get_file(message.photo[-1].file_id)
+    await bot.download_file(file.file_path, "photo.jpg")
