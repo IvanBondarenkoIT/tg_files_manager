97ea0d6a741d6ae1b6a285cfa4e2468aa7b4cc72
Ivan Bondarenko <krenkroyt@gmail.com>
2024-01-20 13:40:10 +0400

    added: interaction done book.csv created

--- parser/parse_pattern.py
+++ parser/parse_pattern.py
@@ -4,23 +4,33 @@ characteristics_file = "characteristics_xpath.csv"
 
 
 class BookParsePattern:
-    def __init__(self):
-        self.book_link = ""
+    def __init__(self, file_path: str):
+        self.file_path = file_path
+        self.book_name_xpath = ""
         self.book_characteristics = []
 
     def read_csv_with_characteristics(self):
-        with open(characteristics_file, "r") as csvfile:
+        with open(self.file_path, "r") as csvfile:
             csv_reader = csv.reader(csvfile)
             for row_number, row in enumerate(csv_reader):
                 key, value = row
                 if row_number == 0:
-                    self.book_link = value
+                    self.book_name_xpath = value
                 else:
-                    self.book_characteristics.append({key: value})
+                    self.book_characteristics.append((key, value))
 
 
-if __name__ == "__main__":
-    book = BookParsePattern()
+def get_book_settings(path: str) -> BookParsePattern:
+    book = BookParsePattern(path)
     book.read_csv_with_characteristics()
-    print(book.book_link)
-    print(book.book_characteristics)
+    return book
+
+
+book_xpath = get_book_settings(characteristics_file)
+
+#
+# if __name__ == "__main__":
+#     book = BookParsePattern()
+#     book.read_csv_with_characteristics()
+#     print(book.book_link)
+#     print(book.book_characteristics)
